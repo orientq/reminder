d9c5bc7186d3853a664e2652cce4f812ad51d1ce
mliqian <qianqianio@qq.com>
2019-09-06 14:31:12 +0800

    Handle add item action

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-console.log('main.js loaded')
 
-function onSubmit(e) {
-  event.preventDefault();
+
+function addItem(e) {
+  e.preventDefault();
+  let input = document.getElementById('content');
+  let value = input.value;
+  let errorMessageDiv = document.getElementById('error-message');
+  if (!value) {
+    errorMessageDiv.innerText  = "Nothing to submit";
+    return;
+  } else {
+    errorMessageDiv.innerText  = "";
+  }
+
+  addToList(value)
+}
+
+
+function addToList(content) {
+  let listUl = document.getElementById('todo-list');
+  let newItem = document.createElement('li');
+  newItem.innerHTML = content;
+  listUl.appendChild(newItem);
 }
